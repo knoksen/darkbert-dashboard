ccc6bf6b1ab52c8e291460db477c5c690601743a
Jarle Taksdal <31391420+knoksen@users.noreply.github.com>
2025-07-16 17:23:20 +0200

    Add root package.json and supporting configs (#12)

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-﻿module.exports = {
-  plugins: {
-    // The new Tailwind PostCSS plugin (wraps the core tailwindcss logic)
-    '@tailwindcss/postcss': {},
-    // Autoprefixer stays the same
-    autoprefixer: {},
-  },
-}
