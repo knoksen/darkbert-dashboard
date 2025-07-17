1c217af6e5add9b8fca9db82d6dc7f73f5bf8836
Jarle Taksdal <31391420+knoksen@users.noreply.github.com>
2025-07-18 00:56:19 +0200

    Integrate metrics API

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
-﻿import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import './styles/index.css'
 import { ThemeProvider } from '@emotion/react'
 import theme from './theme/theme'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
